e1ffa0198ff8d1a6a2b29ba36a2c30444cae5a0f
iqbalpa <iqbalpahlevi16@gmail.com>
2025-03-22 20:12:24 +0700

    create todo

--- va/views.py
+++ va/views.py
@@ -17,3 +17,10 @@ def va_request(request):
     serializer.save()
     return Response(serializer.data, status=status.HTTP_201_CREATED)
   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# TODO: implement voice assistant flow
+# 1. receive audio file and save it to the Google Cloud Storage Bucket (GCS) and return the path
+# 2. send the path to the speech-to-text API and get the text
+# 3. send the text to vertex AI's LLM API and get the answer
+# 4. send the answer to the text-to-speech API and store the audio file to the GCS and return the path
+# 5. return the audio file path
