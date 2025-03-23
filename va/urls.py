5957f5ff0ff69a302249892d9210ee95548998ac
iqbalpa <iqbalpahlevi16@gmail.com>
2025-03-23 12:11:18 +0700

    feat: ensure workflows

--- va/urls.py
+++ va/urls.py
@@ -3,7 +3,7 @@ from va import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('test/', views.index),
+    path('healthcheck/', views.index),
     path('', views.voice_assistant),
 ]
 
