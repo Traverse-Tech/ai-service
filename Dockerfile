b468b5acc91733f09d13e9ce09591f1ea0f6b001
Naila Shafirni Hidayat <naila.shafirni@ui.ac.id>
2025-03-23 19:20:53 +0700

    change port from 8000 to 50051

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-EXPOSE 8000
+EXPOSE 50051
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["python", "grpc_server.py"]
--- client.py
+++ client.py
@@ -2,24 +2,19 @@ import grpc
 from va import va_pb2
 from va import va_pb2_grpc
 
-# Load the gRPC stub
-channel = grpc.insecure_channel("localhost:50051")  # Change port if needed
+channel = grpc.insecure_channel("localhost:50051")
 stub = va_pb2_grpc.VoiceAssistantServiceStub(channel)
 
-# Read an audio file as bytes
-with open("test_audio.wav", "rb") as f:
+with open("temp/test_audio.wav", "rb") as f:
     audio_bytes = f.read()
 
-# Create the request
 request = va_pb2.AudioRequest(
     session_id="test-session-123",
     audio_data=audio_bytes,
 )
 
-# Call the API
 response = stub.ProcessAudio(request)
 
-# Print the response
 print("Session ID:", response.session_id)
 print("Transcribed Text:", response.transcribed_text)
 print("AI Response:", response.ai_response)
