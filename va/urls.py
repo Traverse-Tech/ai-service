966d7ead6444f5f73ae001bd8af51109b4d3d30c
iqbalpa <iqbalpahlevi16@gmail.com>
2025-03-23 10:07:23 +0700

    feat: create the flow

--- va/urls.py
+++ va/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from .views import VoiceAssistantView
 from va import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('test/', views.index),
-    path('/', views.va_request),
     path('upload/', views.upload_file),
+    path("/", VoiceAssistantView.as_view(), name="voice_assistant"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
